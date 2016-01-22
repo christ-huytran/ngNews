c2b14c2d219a6409df3b43233086359b1ae5a6c2
Chris Tran <christopher.huytran@gmail.com>
2016-01-22 15:13:52 -0800

    add ui.router

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-var app = angular.module('ngNews', []);
+var app = angular.module('ngNews', ['ui.router']);
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    })
+
+  $urlRouterProvider.otherwise('home');
+
+}]);
+
 
 app.factory('posts', [function(){
   var o = {
